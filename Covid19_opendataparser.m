f8e9fc56f786410a68d80311bb41693a8fb0a429
WakeNaoki <wakeke.rainbowearth@gmail.com>
2020-04-18 21:53:05 +0900

    Create Covid19_opendataparser.m

---
+++ Covid19_opendataparser.m
@@ -0,0 +1,14 @@
+data = readtable('130001_tokyo_covid19_patients.csv','FileEncoding','UTF-8');
+data.Properties.VariableNames{1} = 'No';
+data.Properties.VariableNames{2} = 'AreaCode';
+data.Properties.VariableNames{3} = 'Prefecture';
+data.Properties.VariableNames{5} = 'Date';
+data.Properties.VariableNames{6} = 'DateIdx';
+data.Properties.VariableNames{8} = 'Area';
+data.Properties.VariableNames{9} = 'Age';
+data.Properties.VariableNames{10} = 'Gender';
+data.Properties.VariableNames{16} = 'IsDischarged';
+
+for i = 1:size(data,1)
+    data.Date{i} = datenum(data.Date{1});
+end
